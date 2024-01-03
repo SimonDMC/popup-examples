70f81a050d153cbe3ad3b97e38a6ddbebb1a59c2
SimonDMCPlayer <46278840+SimonDMCPlayer@users.noreply.github.com>
2024-01-03 21:01:07 +0100

    Update download popup to match the website

--- examples/download.js
+++ examples/download.js
@@ -2,8 +2,8 @@ const popup = new Popup({
     id: "download",
     title: "Download Instructions",
     content: `
-        1. Download and install the {a-http://example.com}[CliCli] launcher.
-        2. Navigate to the {a-http://example.com}[game page] and click 'Open CliCli Platform'.
+        1. Download and install the {a-http://example.com}[launcher].
+        2. Navigate to the {a-http://example.com}[game page] and click 'Open Platform'.
         3. Press the 'Start Game' button to download and launch the game.
         `,
     backgroundColor: "#000",
