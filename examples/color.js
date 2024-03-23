f333ade460957945534c78a5bf44d75c9e28f341
SimonDMC <simondmcplayer@gmail.com>
2024-03-23 15:23:20 +0100

    Update color example popup to look better on mobile

--- examples/color.js
+++ examples/color.js
@@ -7,7 +7,7 @@ const popup = new Popup({
         Six characters, ranging from 00-FF for 3 channels.
         Values are in {a-http://example.com}[Base-16].
         big-margin§{bold}[#{red}[E4]{green}[F2]{blue}[DB]]
-        big-margin§{black bold}[#000000] is black.                {white bold shadow}[#FFFFFF] is white.
+        big-margin§{black bold}[#000000] is black.             {white bold shadow}[#FFFFFF] is white.
         big-margin§Good luck.`,
     titleColor: "rgb(92, 0, 95)",
     titleMargin: "0",
